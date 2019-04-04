2c4a780bbc8c0a293b0571c83390b03e02019304
Isabella Isaac Fernandes <isabella.isaaacfernandes@gmail.com>
2019-04-04 13:44:55 -0300

    Projeto Catalogo de Bebidas - Backend

---
+++ CatalogoApi/Controllers/BebidasController.cs
@@ -0,0 +1,105 @@
+using System.Collections.Generic;
+using CatalogoApi.Models;
+using CatalogoApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BebidasController : ControllerBase
+    {
+        private readonly BebidaService _bebidaService;
+
+        public BebidasController(BebidaService bebidaService)
+        {
+            _bebidaService = bebidaService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Bebida>> Listar()
+        {
+            return _bebidaService.Listar();
+        }
+
+        [HttpGet("{id:length(24)}", Name = "ListarBebida")]
+        public ActionResult<Bebida> Listar(string id)
+        {
+            var bebida = _bebidaService.Listar(id);
+
+            if (bebida == null)
+            {
+                return NotFound();
+            }
+
+            return bebida;
+        }
+
+        [HttpGet("cervejas", Name = "ListarCervejas")]
+        public ActionResult<List<Bebida>> ListarCervejas()
+        {
+            var bebida = _bebidaService.ListarPor("CERVEJA");
+
+            if (bebida == null)
+            {
+                return NotFound();
+            }
+
+            return bebida;
+        }
+
+        [HttpGet("cachacas", Name = "ListarCachacas")]
+        public ActionResult<List<Bebida>> ListarCachacas()
+        {
+            var bebida = _bebidaService.ListarPor("CACHACA");
+
+            if (bebida == null)
+            {
+                return NotFound();
+            }
+
+            return bebida;
+        }
+        
+        [HttpPost("cadastrar", Name = "Cadastrar")]
+        public ActionResult<Bebida> Cadastrar(Bebida bebida)
+        {
+            _bebidaService.Cadastrar(bebida);
+
+            if(bebida.Tipo == "CERVEJA"){
+                return CreatedAtRoute("ListarCerveja", new { id = bebida.Id.ToString() }, bebida);
+            }
+            return CreatedAtRoute("ListarCachaca", new { id = bebida.Id.ToString() }, bebida);
+        }
+
+        [HttpPut("editar/{id:length(24)}", Name = "Editar")]
+        public IActionResult Editar(string id, Bebida novaBebida)
+        {
+            var bebida = _bebidaService.Listar(id);
+
+            if (bebida == null)
+            {
+                return NotFound();
+            }
+
+            _bebidaService.Editar(id, novaBebida);
+
+            return NoContent();
+        }
+
+        [HttpDelete("remover/{id:length(24)}", Name = "Remover")]
+        public IActionResult Remover(string id)
+        {
+            var bebida = _bebidaService.Listar(id);
+
+            if (bebida == null)
+            {
+                return NotFound();
+            }
+
+            _bebidaService.Remover(bebida.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ CatalogoApi/Services/BebidaService.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CatalogoApi.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace CatalogoApi.Services
+{
+    public class BebidaService
+    {
+        private readonly IMongoCollection<Bebida> _bebidas;
+
+        public BebidaService(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("CatalogoDb"));
+            var database = client.GetDatabase("CatalogoDb");
+            _bebidas = database.GetCollection<Bebida>("Bebidas");
+        }
+
+        public List<Bebida> Listar()
+        {
+            return _bebidas.Find(bebida => true).ToList();
+        }
+
+        public Bebida Listar(string id)
+        {
+            return _bebidas.Find<Bebida>(bebida => bebida.Id == id).FirstOrDefault();
+        }
+
+        public List<Bebida> ListarPor(string tipo)
+        {
+            return _bebidas.Find<Bebida>(bebida => bebida.Tipo == tipo).ToList();
+        }
+
+        public Bebida Cadastrar(Bebida bebida)
+        {
+            _bebidas.InsertOne(bebida);
+            return bebida;
+        }
+
+        public void Editar(string id, Bebida bebidaNova)
+        {
+            _bebidas.ReplaceOne(bebida => bebida.Id == id, bebidaNova);
+        }
+
+        public void Remover(Bebida bebidaExcluida)
+        {
+            _bebidas.DeleteOne(book => book.Id == bebidaExcluida.Id);
+        }
+
+        public void Remover(string id)
+        {
+            _bebidas.DeleteOne(bebida => bebida.Id == id);
+        }
+    }
+}
