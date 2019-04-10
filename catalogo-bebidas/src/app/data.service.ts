c0f99fc590bcac7101439b4fe283f78e71cfd9b3
Victor Mourthe <victor.mourthe@dtidigital.com.br>
2019-04-10 14:26:31 -0300

    Alteracao no Card - Cerveja

--- catalogo-bebidas/src/app/data.service.ts
+++ catalogo-bebidas/src/app/data.service.ts
@@ -7,11 +7,13 @@ import { pipe } from "@angular/core/src/render3";
 })
 export class DataService {
   baseUrl: string = "https://localhost:5001/api/";
+  urlTest: string = "http://localhost:3000/cervejas";
 
   constructor(private httpClient: HttpClient) {}
 
   get_cervejas() {
     return this.httpClient.get(this.baseUrl + "bebidas/cervejas");
+    //return this.httpClient.get(this.urlTest);
   }
 
   get_cachacas() {
